cd5d02e68f65fa3c86512793fe9ad702182c72f5
cleiton_felipe <cleiton@bradesco.com.br>
2017-12-29 11:54:39 -0200

    Initial Commit

---
+++ Threads/Chapter_1_Listing_1_5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace Chapter_1_Listing_1_5
+{
+    class Program
+    {
+        [ThreadStatic]
+        public static int _field;
+        static void Main(string[] args)
+        {
+            new Thread(() => {
+                for (int i = 0; i < 10; i++)
+                {
+                    _field++;
+                    Console.WriteLine("Thread A: {0}", _field);
+                }
+            }).Start();
+
+            new Thread(() => {
+                for (int x = 0; x < 10; x++)
+                {
+                    _field++;
+                    Console.WriteLine("Thread B: {0}", _field);
+                }
+            }).Start();
+
+            Console.ReadKey();
+        }
+    }
+}
