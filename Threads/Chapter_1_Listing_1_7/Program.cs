5822ef70b5a57ec55cfd88f93c8a44ff52c6bd2c
cleiton_felipe <cleiton@bradesco.com.br>
2018-01-02 16:53:11 -0200

    Add ThreadPool e ajuste distribuição e nomes

---
+++ Threads/Chapter_1_Listing_1_7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Chapter_1_Listing_1_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem((s) => {
+                Console.WriteLine("Working on a thread from threadpool");
+            });
+            Console.ReadKey();
+        }
+    }
+}
