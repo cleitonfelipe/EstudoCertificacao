7506e77eb59c5e8c21a7fec024f9ad42ad307d72
cleiton_felipe <cleiton@bradesco.com.br>
2018-01-02 17:00:43 -0200

    Alteração de pastas

---
+++ Chapter_1_Listing_1_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chapter_1_Listing_1_1
+{
+    class Program
+    {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(0);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.Start();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main Thread: Do some work");
+                Thread.Sleep(0);
+            }
+
+            t.Join();
+
+            //Console.ReadKey();
+        }
+    }
+}
