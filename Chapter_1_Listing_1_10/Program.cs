44e61b35834a8575495dcf2dd8d231282e8780c3
Cleiton Felipe de Moraes <cleiton.felipe@yahoo.com.br>
2019-09-08 14:55:45 -0300

    Add Listing 10 and 11

---
+++ Chapter_1_Listing_1_10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_1_Listing_1_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> t = Task.Run(() =>
+            {
+                return 42;
+            }).ContinueWith((i) =>
+            {
+                return i.Result * 2;
+            });
+
+            Console.WriteLine(t.Result);
+        }
+    }
+}
